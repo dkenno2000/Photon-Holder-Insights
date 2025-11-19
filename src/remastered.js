fb50b6cd9504731f118a13c275b02c9c0d14a6c4
dkenno2000 <d.kenno2000@gmail.com>
2025-11-19 02:38:44 +0100

    remastered.js

--- src/remastered.js
+++ src/remastered.js
@@ -1,3 +1,4 @@
+// make sure you have amcharts5 in your "node_modules" folder before bundling
 import * as am5 from "@amcharts/amcharts5";
 import * as am5percent from "@amcharts/amcharts5/percent";
 import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
@@ -812,4 +813,5 @@ window.onload = async () => {
     }
     TooltipPhoton2();
     SolPrice();
+
 };
